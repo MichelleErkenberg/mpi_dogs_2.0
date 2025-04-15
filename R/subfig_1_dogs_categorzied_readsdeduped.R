616d1a3531bb74ddfcc1363bb67a11510385c9ee
Michelle Erkenberg <erkenberg.michelle@gmail.com>
2025-04-15 16:29:32 +0300

    added extra ReadsDeduped graphic to subfig_1

--- R/subfig_1_dogs_categorzied_readsdeduped.R
+++ R/subfig_1_dogs_categorzied_readsdeduped.R
@@ -1,6 +1,8 @@
 library(data.table)
 library(ggplot2)
 library(dplyr)
+library(cowplot)
+
 
 # Set working directory
 setwd("~/github/mpi_dogs_2.0/")
@@ -23,11 +25,14 @@ reads_threshold <- 50
 dt.filter_valid <- dt.filter[Family == "Canidae" & ReadsDeduped >= reads_threshold]
 
 
-# Get the list of sample_ids that meet the criteria
+# Get the list of sample_ids and readsdeduped that meet the criteria
 valid_samples <- dt.filter_valid$sample_id
 
-# Filter the main data to include only valid samples
+# Filter the main data to include only valid samples and add + rename ReadsDeduped
 dt.main_filtered <- dt.main[sample_id %in% valid_samples]
+dt.filter_valid_reads <- dt.filter_valid[sample_id %in% valid_samples]
+dt.main_filtered_reads <- mutate(dt.main_filtered, dt.filter_valid_reads$ReadsDeduped)
+names(dt.main_filtered_reads)[names(dt.main_filtered_reads) == "dt.filter_valid_reads$ReadsDeduped"] <- "ReadsDeduped"
 
 # Define custom order for locations
 custom_order <- c("Dog Office (Container)", "Dog Office (Lily/ThorA)", "Hallway (Lily/ThorA)", 
@@ -36,10 +41,11 @@ custom_order <- c("Dog Office (Container)", "Dog Office (Lily/ThorA)", "Hallway 
                   "Negativ Control")
 
 # Merge main data with categories
-dt.combined <- merge(dt.main_filtered, dt.categories, by = "sample_id", all.x = TRUE)
+dt.combined <- merge(dt.main_filtered_reads, dt.categories, by = "sample_id", all.x = TRUE)
+dt.combined_reads <- select(dt.combined, sample_id, location, ReadsDeduped)
 
 # Reshape data to long format
-dt.long <- melt(dt.combined, id.vars = c("sample_id", "location"), 
+dt.long <- melt(dt.combined, id.vars = c("sample_id", "location", "ReadsDeduped"), 
                 variable.name = "dog", value.name = "value")
 
 # Extract number from sample_id for sorting
@@ -50,6 +56,7 @@ setorder(dt.long, location, sample_number)
 
 # Apply custom order to location factor
 dt.long$location <- factor(dt.long$location, levels = custom_order)
+dt.combined_reads$location <- factor(dt.combined_reads$location, levels = custom_order)
 
 # Create the bar plot with asterisks for NA values
 p <- ggplot() +
@@ -69,45 +76,31 @@ p <- ggplot() +
 
 print(p)
 
-
-# Save the plot
-ggsave("figures/subfig_1.png", p, width = 24, height = 20, limitsize = FALSE)
-
-
-# --------------new plot with Readsdeduped -----------------
-
-# create file for further manual modifications
-dt.reads_merge <- merge(dt.combined, dt.filter_valid, by = "sample_id", all.x = TRUE)
-write.csv(dt.reads_merge, "data/dog_samples/R_prep/all_dogs_with_ThorA_Cami_without_Lily/dt.reads_merge.csv") #just modify it to show ReadsDeduped in plot
-
-
-dt_reads <- fread('data/dog_samples/R_prep/all_dogs_with_ThorA_Cami_without_Lily/R_prep_sample_vs_dog_all_dogs_with_ThorA_Cami_without_Lily_10snp_mod.csv', na.strings = c('-', 'NA', ''))
-
-custom_order <- c("Dog Office (Container)", "Dog Office (Lily/ThorA)", "Hallway (Lily/ThorA)", "Dog Office (Anda/Charlie)", "Hallway (Anda/Charlie)", "Non-Dog Office", "Elevator", "Main Entrance", "Lab (PCR Lab)", "Lab (Cleanroom)", "Negativ Control")
-
-dog_columns <- setdiff(names(dt_reads), c("sample_id", "location"))
-
-dt_reads.long <- melt(dt_reads, id.vars = c("sample_id", "location"), 
-                      variable.name = "dog", value.name = "value")
-
-dt_reads.long[, sample_number := as.integer(sub("sample_(\\d+)\\(.*", "\\1", sample_id))]
-
-dt_reads.long$location <- factor(dt_reads.long$location, levels = custom_order)
-
 p_reads <- ggplot() +
   # Add bars only for non-NA values
-  geom_bar(data = dt_reads.long[!is.na(value)], aes(x = factor(sample_id), y = value, fill = dog), 
+  geom_bar(data = dt.combined_reads, aes(x = factor(sample_id), y = ReadsDeduped), 
            stat = "identity", position = "dodge") +
-  # Add asterisks for NA values at the correct positions
-  geom_text(data = dt_reads.long[is.na(value)], aes(x = factor(sample_id), y = 0, label = "*"), 
-            position = position_dodge(width = 0.9), vjust = -0.5) +
-  facet_grid(dog ~ location, scales = "free_x", space = "free_x") +
+  facet_grid( ~ location, scales = "free_x", space = "free_x") +
   theme_bw() +
-  labs(title = "Dog Values by Sample and Location",
-       x = "Sample ID", y = "Value", fill = "Dog") +
+  labs(title = "ReadsDeduped by Sample and Location (Filtered)",
+       x = "Sample ID", y = "ReadsDeduped") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
         strip.text = element_text(size = 10, face = "bold"),
-        legend.position = "none")
+        legend.position = "none") 
+
 print(p_reads)
 
-ggsave("figures/subfig_1.png", p_reads, width = 24, height = 20, limitsize = FALSE)
+#both plots in one graphic
+p_grid <- plot_grid(p, p_reads,
+          labels = "AUTO",
+          nrow = 2,    #diagramms in two rows
+          align = "v",
+          axis = "lr", # alignment along left and right (x-axis)
+          rel_heights = c(1/2, 1/3)) # heights of the diagramms
+
+print(p_grid)
+
+# Save the plot
+ggsave("figures/subfig_1.png", p_grid, width = 24, height = 20, limitsize = FALSE)
+
+
